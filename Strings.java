901754495d921272e994c0078690f30b15ca4e23
Ravi Mahar <ravimahar12321@gmail.com>
2025-10-10 08:47:10 +0530

    Strings and Wrappers

---
+++ Strings.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Strings{
+
+    void checkReference(String a,String b){
+        if(a==b){
+            System.out.println("same");
+        }
+        else{
+            System.out.println("different Pool");
+        }
+
+    }
+
+    void checkEqualMethod(String a,String b){
+        
+        if(a.equals(b)){
+            System.out.println("same Data ");
+        }
+        else{
+            System.out.println("Different Data");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int _ = 10;
+        String name = "Ravi"; // stored in string pool in heap memory
+        System.out.println(name);
+        String a  = new String("ravi");
+        String b = new String("ravi");
+
+        Integer e = 10;
+        Scanner s = new Scanner(System.in);
+        // int data = s.nextInt();
+        System.out.println(Arrays.toString(new int[]{1,2,3,4}));
+        s.close();
+        char chS[] = a.toCharArray();
+        System.out.println(Arrays.toString(chS));
+
+
+        //for concatination-> the charater get calculated as ASCII value, String will Get concatinated as it is
+        // If we add int with String the integer will converted into wrapper class and .toString method will be used and it will be concatinated with string
+
+        // . stripe() use to remove extra space, string.split(from)
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('a');
+                
+    }
+}
---
+++ Wrap.java
@@ -0,0 +1,25 @@
+public class Wrap {
+    public static void main(String[] args) {
+        //allow primited values to used as object
+        int d = 20;
+
+        //Autoboxing
+        Integer c = d;
+        Integer a = 1;
+        Float b = 10.2f;
+
+
+        //unboxing
+        int x = a;
+
+        //using .toString in the object converts them to string
+        // we can convert string to primitive by using .parseInt, boolean, Double etc
+
+        
+
+
+
+
+    }
+    
+}
