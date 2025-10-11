473c12f7bdcb2785f5532c9783adb626423fa36c
getthecodeoutofit <ravimahar12321@gmai.com>
2025-10-11 22:42:08 +0530

    Multi Threading

---
+++ MThread.java
@@ -0,0 +1,34 @@
+class MThread{
+    public static void main(String []arg){
+        A aobj = new A();
+        B bobj = new B();
+        Thread t = new Thread(bobj);
+        aobj.start();
+        t.start();
+        
+        
+    } 
+}
+
+
+class A extends Thread{
+    @Override
+    public void run(){
+        for(int i=0;i<100;i++){
+            System.out.println("Hello ");
+        }     
+    }
+    
+    
+}
+
+class B implements Runnable{
+    @Override
+    public void run(){
+        for(int i=0;i<190;i++){
+            System.out.println("world");
+        }
+        
+    }
+    
+}
