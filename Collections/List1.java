e702a966b5eda41e56e82299053252c4685947f2
getthecodeoutofit <ravimahar12321@gmail.com>
2025-12-27 22:28:14 +0530

    Collections -LinkedList

--- Collections/List1.java
+++ Collections/List1.java
@@ -3,9 +3,16 @@ import java.util.*;
 
 public class List1 {
     public static void main(String[] args) {
-        List<Integer> l = new LinkedList<>();
+
+        List<Integer> l = new LinkedList<>(); //each node is created dynamically  unlike arraylist having intial capacity
+        //Double linked list
+
+
+
         List<Integer> a = new ArrayList<>(); //more Prefered Universallt used Interface(List)
-        ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(1,2));
+        ArrayList<Integer> a2 = new ArrayList<>(Arrays.asList(1,2)); //provide the initial capacity to the array when it fill it create another array and transfer it to another which laterer cause the overhead
+        //stored in heap, logically contiguous but not physically it a array of references which refers to the integer at any specific location
+        // obj-> [ref1][ref2][ref3]
 
         a.add(100); // indexing also works a.get()
         int lenA = a.size();
@@ -17,6 +24,10 @@ public class List1 {
 
         a.set(0, 9);
         System.out.println(a.get(0));
+
+        l.add(10);
+
+
          
 
 
